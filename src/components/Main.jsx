b184efcded668496a83d552d6c097e0a81ade1b2
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-05-21 16:49:21 +0300

    commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./main.css"
+export default class Main extends Component {
+  render() {
+    return (
+        <>
+        <div className='main'>
+        < img class="image"src="https://wallpapercave.com/wp/wp3279348.jpg" alt="fjsadkj"/>
+        </div>
+        </>
+      
+    )
+  }
+}
