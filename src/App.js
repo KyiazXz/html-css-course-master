e222236e3b1dce7d50d8ca47a4fd385e5e2539e5
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-05-23 16:28:52 +0300

    section2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+// import Section1  from './sections/section1/Section1';
 import Section2 from './sections/section2/Section2';
 // import Chalange1 from './sections/section1/Chalange1';
 
