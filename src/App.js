bf1f35c23894e367e47b8689aa901d2182d4e847
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-05-23 16:26:59 +0300

    section2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Section1 from './sections/section1/Section1';
-import Chalange1 from './sections/section1/Chalange1';
+import Section2 from './sections/section2/Section2';
+// import Chalange1 from './sections/section1/Chalange1';
 
 function App() {
   return (
     <>
     {/* <Section1/> */}
-    <Chalange1/>
+    {/* <Chalange1/> */}
+    <Section2 />
     </>
   );
 }
---
+++ src/sections/section2/Section2.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./section2.css"
+export default function Section2() {
+  return (
+    <main>
+        <div className='container'>
+        <nav> <a href='https://wallpapercave.com/w/uwp3709822'><h1> The Code Magazine</h1></a></nav>
+             
+      <h3> The Basic Language of the Web: HTML</h3>
+      <img src='https://wallpapercave.com/uwp/uwp3722856.jpeg' alt='img'/>
+      <p>lorem</p>
+    
+        <div className='colors'>
+            <li className='first'></li>
+            <li className='sec'></li>
+            <li className='third'></li>
+            <li className='four'></li>
+            <li className='five'></li>
+        </div>
+
+        <div className='sale'>
+            <h3>sale</h3>
+        </div>
+
+
+      
+      
+        <button>add me</button>
+      
+      </div>
+    </main>
+  )
+}
