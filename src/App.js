13f8c880c0c88a9adea17c7ec52ac172c648f1af
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-05-25 18:01:29 +0300

    section3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 // import Section1  from './sections/section1/Section1';
-import Section2 from './sections/section2/Section2';
+// import Section2 from './sections/section2/Section2';
 // import Chalange1 from './sections/section1/Chalange1';
+import Section3 from './sections/section3/Section3';
+
 
 function App() {
   return (
     <>
     {/* <Section1/> */}
     {/* <Chalange1/> */}
-    <Section2 />
+    {/* <Section2 /> */}
+    <Section3 />
     </>
   );
 }
