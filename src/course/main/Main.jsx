10d52c0ea9d1955b6746f4f74b2684a059f66c59
KYIAZBEK ZHOLDOSHEV <111270349+KyiazXz@users.noreply.github.com>
2023-07-10 02:07:58 +0300

    10%

--- src/course/main/Main.jsx
+++ src/course/main/Main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { BsFillPlayCircleFill} from 'react-icons/bs';
 import {IconContext} from "react-icons";
+import LearnAnything from './secondPage/LearnAnything';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
+
 
 import "./main.css"
 export default function Main() {
   return (
 
     <>
+    <div className='main'>
+
     <div className='main-container grid grid-2-cols'>
         <div className='title-container flex '>
             <div>
@@ -16,7 +22,7 @@ export default function Main() {
             <h1>Forward</h1>
             </div>
             <div className='subheading-container'>
-
+              
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque, cumque! Ipsam harum eum accusamus! Corporis asperiores neque ea ab natus rem, dolor cupiditate a quasi, laboriosam officiis cum suscipit id!</p>
             </div>
 
@@ -25,10 +31,19 @@ export default function Main() {
                 <div className='icon-container flex'>
                 <IconContext.Provider value={{className:"myReact-icons"}}>
                 <BsFillPlayCircleFill/>
-  </IconContext.Provider>
+                </IconContext.Provider>
                 </div>
                 <p> Watch Into</p>
             </div>
+<div className='avatar-container'>
+
+<AvatarGroup total={24}>
+  <Avatar alt="Remy Sharp" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5QI9SMmsJ-spISijcgGAjClqfAlrZs9y-nV0hMFlE-4KnjwuuWdzzOTQfwlFVQnSsa1E&usqp=CAU" />
+  <Avatar alt="Travis Howard" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIlnYvbq5Kx-XcQ7nAmKe12T6sCBTz3-JNM8fwEfb-3z9eCLgUKfeWWV9q9BbJCJWCmRQ&usqp=CAU" />
+  <Avatar alt="Agnes Walker" src="https://images.unsplash.com/photo-1514846326710-096e4a8035e0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8c3R1ZGVudCUyMGZhY2V8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=300&q=60" />
+  <Avatar alt="Trevor Henderson" src="https://images.unsplash.com/photo-1474176857210-7287d38d27c6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHN0dWRlbnQlMjBmYWNlfGVufDB8fDB8fHww&auto=format&fit=crop&w=300&q=60" />
+</AvatarGroup>
+</div>
             </div>
 
             
@@ -41,7 +56,11 @@ export default function Main() {
             </div>
         </div>
     </div>
+              </div>
 
+
+
+        <LearnAnything/>
     </>
   )
 }
---
+++ src/course/main/secondPage/LearnAnything.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import { ImBooks} from 'react-icons/im';
+import {IconContext} from "react-icons";
+import { TbCertificate} from 'react-icons/tb';
+import {FaChalkboardTeacher} from 'react-icons/fa';
+
+export default function LearnAnything() {
+  return (
+    <div className='learn'>
+
+    <div className=' main-container grid grid-2-cols'>
+        <div className='learn-img-container margin-left'> 
+        <img src='./course/boy.png' alt='boy reading book' />
+        </div>
+        <div className='learn-topics-container'>
+          <div>
+            <p className='learn-topics-title'>learn anything</p>
+            <h1 className='learn-topics-header'> Benefits About Online</h1> <h1 className='learn-topics-header-2'>Learning Expertise</h1>
+          </div>
+
+          <div className='topics flex gap'>
+            <div className='topic flex gap'>
+                  <div className='icon-cont'>
+                  <IconContext.Provider value={{className:"learn-icon"}}>
+                    <ImBooks/> 
+                    </IconContext.Provider>
+                    </div>
+                  <div className='topic-name'>
+                    <h1>Online Courses</h1>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalla and Consonatia, there live the blind texts.</p>
+                  </div>
+            </div>
+      
+            <div className='topic flex gap'>
+                  <div className='icon-cont'>
+                  <IconContext.Provider value={{className:"learn-icon"}}>
+                    <TbCertificate/> 
+                    </IconContext.Provider>
+                    </div>
+                  <div className='topic-name'>
+                    <h1>Earn A Certificates</h1>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalla and Consonatia, there live the blind texts.</p>
+                  </div>
+            </div>
+
+            <div className='topic flex gap'>
+                  <div className='icon-cont'>
+                  <IconContext.Provider value={{className:"learn-icon"}}>
+                    <FaChalkboardTeacher/> 
+                    </IconContext.Provider>
+                    </div>
+                  <div className='topic-name'>
+                    <h1>Learn with Expert</h1>
+                    <p>Far far away, behind the word mountains, far from the countries Vokalla and Consonatia, there live the blind texts.</p>
+                  </div>
+            </div>
+
+          </div>
+
+        </div>
+    </div>
+    </div>
+  )
+}
